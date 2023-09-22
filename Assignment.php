3316a42a3c415e636aafeaed47f11b30c789bd76
iwodi5050 <iwodiemmanueljacob@gmail.com>
2023-09-22 06:13:48 -0700

    design

---
+++ Assignment.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <form action="#" method="post">
+            <fieldset>
+                <legend>submith</legend>
+
+                <label for="name">first name</label>
+                <input type="text" name="firstname" id="firstname">
+
+
+                 <label for="middle name">middle name</label>
+                 <input type="text" name="middlename" id="name">
+
+                 <label for="surname">surname</label>
+                 <input type="text" name="surname" id="surname">
+
+                 <label for="phone_number">phone number</label>
+                 <input type="number" name="phone_number" id="phone_number">
+
+                 <label for="age">age</label>
+                 <input type="range" name="age" id="age">
+
+                 <div>
+                    <input type="checkbox" name="cop">
+                 </div>
+
+            </fieldset>
+
+        </form>
+    </section>
+</body>
+</html>
